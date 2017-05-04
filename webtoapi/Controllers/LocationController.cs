081fde8f96e724c1635fb283237dd61777d7d659
Wasif.Ali <Wasif.Ali@WASIF-ALI>
2017-05-04 15:33:57 +0500

    hk

---
+++ webtoapi/Controllers/LocationController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace webtoapi.Controllers
+{
+    public class LocationController : ApiController
+    {
+        //https://localhost:44375/api/location?cityName=abc
+        public Models.Location GetLocation(string cityName)
+        {
+            return new Models.Location() { Latitude = 10, Longitude = 20 };
+        }
+    }
+}
---
+++ webtoapi/Models/Location.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webtoapi.Models
+{
+    public class Location
+    {
+        public int Longitude { get; set; }
+        public int Latitude { get; set; }
+    }
+}
